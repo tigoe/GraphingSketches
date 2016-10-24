2393d498599a7a22ab7cd707495fa4f81c79a200
tigoe <tom.igoe@gmail.com>
2016-10-24 18:12:18 -0400

    Updated RunningGraph and MultiValueGraph

--- RunningGraph/sketch.js
+++ RunningGraph/sketch.js
@@ -1,14 +1,15 @@
 /*
   Serial graphing sketch
-  
+
   Expects an ascii-encoded, followed by carriage return
-  and newline. 
-  
+  and newline.
+
   Works with Shawn Van Every's p5.serialport library for p5.js and
   p5.serialserver library for node.js (https://github.com/vanevery/p5.serialport)
-  
-  created by Tom Igoe
-  12 Oct 2015
+
+  created 12 Oct 2015
+  modified 24 Oct 2016
+  by Tom Igoe
 */
 
 var serial = new p5.SerialPort();     // make an instance of the SeialPort library
@@ -16,55 +17,48 @@ var readings = [];                    // array to hold raw sensor readings
 var lastReading = 0.0;                // last mapped sensor reading (for graphing)
 var minValue = 0;                     // minimum sensor value. Adjust to fit your needs
 var maxValue = 1023;                  // maximum sensor value. Adjust to fit your needs
-var portName = '/dev/cu.usbmodem1411'; // change to the name of your serial port
+//var portName = '/dev/cu.usbmodem1411'; // change to the name of your serial port
+var portName = '/dev/tty.usbmodem142431'; // change to the name of your serial port
 
 function setup() {
   createCanvas(800, 600);             // set the canvas size
-  serial.on('data', serialEvent);     // set the serial data callback 
+  serial.on('data', serialEvent);     // set the serial data callback
   serial.on('error', serialError);    // set the serial error callback
   serial.open(portName);              // open the serial port
   serial.clear();                     // clear the serial buffer
 }
 
 function draw() {
-  var thisReading;    // current reading being plotted
-  var thisAverage;    // current average being plotted
-  var xPos;           // current x position
-  
-   // clear the background:
+  var thisReading; // current reading being plotted
+  var xPos;        // current x position
+
+  // clear the background:
   background(0);
-  // iterate over the width of the canvas:
-  for (xPos = 0; xPos < width; xPos++) {
-    // if the array is longer than the canvas is wide, 
-    // only plot the last <width> elements of the array:
-    if (readings.length > width) {
-      // get the current reading and average:
-      thisReading = readings[readings.length - width + xPos];
-    } else {
-      // if the array is shorter than the width of the canvas,
-      // then xPosition = array position:
-      thisReading = readings[xPos];
+  for (xPos = 1; xPos < width; xPos++) {
+    thisReading = readings[xPos];     // get the current reading
+    lastReading = readings[xPos - 1]; // get the last reading
+    if (thisReading) {                 // if there's a valid reading,
+    stroke(0);       // Black out previous reading text
+    fill(0);
+    text(lastReading, 30, 30);
+    fill(170, 89, 255); // set fill for  current reading text
+    text(thisReading, 30, 30);
+          // set stroke color for the graph lines:
+        stroke(170, 89, 255);
+        // calculate the current and previous Y positions:
+        var yPos = map(thisReading, minValue, maxValue,
+          height, 0);
+        var lastYPos = map(lastReading, minValue, maxValue,
+          height, 0);
+        // draw a line from the last position to the current one:
+        line(xPos - 1, yPos, xPos,lastYPos);
     }
-    
-    // map the current reading and average to the canvas height:
-    var readingY = height - map(thisReading, minValue, maxValue, 0, height);
-    // only draw if you've the xPosition > 0:
-    if (xPos > 0) {
-      // blue for the reading:
-      stroke(0, 127, 255);
-      line(xPos - 1, lastReading, xPos, readingY);
-     }
-    
-    // if the array is at least twice the width of the canvas,
+    // if the array is the width of the canvas,
     // start deleting elements from the front of the array
     // to save memory:
-    if (readings.length > width * 2) {
-      readings.shift();   // delete the first element in the array
+    if (readings.length > width) {
+      readings.shift(); // delete the first element in the array
     }
-    
-    // save the current reading and average as the starting point
-    // for the line in the next iteration:
-    lastReading = readingY;
   }
 }
 
